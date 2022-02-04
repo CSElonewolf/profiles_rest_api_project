5ac850245dde7ed32e832e60afdb9dc7131800d8
CSELonewolf <devkantgorai1@gmail.com>
2022-02-04 05:52:09 +0530

    Create ProfileFeedItem model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
+from django.conf import settings
 
 
 class UserProfileManager(BaseUserManager):
@@ -52,4 +53,18 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
 		"""Return a string representation of our user"""
 		return self.email
 
+
+class ProfileFeedItem(models.Model):
+	""" Profile status update """
+	user_profile = models.ForeignKey(
+		settings.AUTH_USER_MODEL,
+		on_delete= models.CASCADE
+	)
+	status_text = models.CharField(max_length=255)
+	craeted_on = models.DateTimeField(auto_now_add=True)
+
+
+	def __str__(self):
+		"""Return the model as string """
+		return self.status_text
 
